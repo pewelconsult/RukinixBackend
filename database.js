8c602c7fbb6c6b5dce5de1d85d9b27da2a98d992
Newton <pewelconsult@gmail.com>
2024-12-21 05:50:28 +0000

    222

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -16,9 +16,12 @@ const firebaseConfig = {
   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
 };
 
+jsonConfigObject = JSON.stringify(firebaseConfig)
+
+console.log(jsonConfigObject)
 
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig)
+    credential: admin.credential.cert(jsonConfigObject)
 });
 
 
